0ec2277d591e10f4235f9520b2bdf198fa440cb2
unknowns <unknowns0074@gmail.com>
2022-10-29 19:30:28 -0300

    finishing sdk-js v1.0

--- libs/index.js
+++ libs/index.js
@@ -6,17 +6,39 @@ import { getUnixTime } from "./modules/time.js";
 
 const { MD5 } = cryptoJs;
 
-function UNKApp({ name, secretKey }) {
+function UNKApp({ name, secretKey, verifyLicense, verificationFail }) {
 	this.name = name;
 	this.token = MD5(secretKey);
 	this.login = loginWithLicense;
+	this.verify = verifyLicense;
+	this.failCb = verificationFail;
 }
 
-export const initializeApplication = ({ name, secretKey }) => {
-	return new UNKApp({ name: name, secretKey: secretKey });
+/**
+ * Initialize UNKCode app
+ *
+ * @param {Object} config Array with app configuration
+ * @param {Object} config.name The app name as it appears on https://unkcode.com/panel/dev/applications
+ * @param {Object} config.secretKey The app secret key
+ * @param {Object} config.verificationFail Callback that will be executed on license verifiaction fail
+ * @returns {UNKApp} Object to access sdk functions.
+ */
+
+export const initializeApplication = ({ name, secretKey, verifyLicense, verificationFail }) => {
+	if (!name || !secretKey || !verifyLicense || !verificationFail) throw new Error("[UNKCode][ERROR] All fields must be completed to initilize the application.");
+
+	return new UNKApp({ name: name, secretKey: secretKey, verifyLicense: verifyLicense, verificationFail: verificationFail });
 };
 
-async function loginWithLicense({ license, macAddress }) {
+/**
+ * Signin user to app using UNKCode's lincese and if required the macAdress
+ *
+ * @param {Object} config Array with needed data to login
+ * @param {Object} config.license The user license
+ * @param {Object} config.macAddress The user macAdress. Only in case that app config requires it
+ * @returns {boolean} bool value of success
+ */
+async function loginWithLicense({ license, macAddress = undefined }) {
 	const powRes = await axios({
 		url: `https://unkcode.com/api/v1/verify/pow/${this.name}`,
 		method: "POST",
@@ -30,10 +52,15 @@ async function loginWithLicense({ license, macAddress }) {
 
 	let powKey = powRes.data.otherData + MD5(license + this.token).toString();
 
-	let json = JSON.stringify({
+	let json = {
 		token: powKey,
-		macHash: MD5(macAddress).toString(),
-	});
+	};
+
+	if (macAddress) {
+		json.macHash = MD5(macAddress).toString();
+	}
+
+	json = JSON.stringify(json);
 
 	const { data } = await axios({
 		url: `https://unkcode.com/api/v1/verify/${this.name}/${license}`,
@@ -49,12 +76,25 @@ async function loginWithLicense({ license, macAddress }) {
 	let date = await getUnixTime();
 
 	if (date.getUTCMinutes() >= 55 || date.getUTCMinutes() <= 5) {
-		date.setTime(date.getTime() + 60 * 60 * 1000);
+		date.setTime(date.getTime() + 3600000);
 	}
 
-	if (MD5(powKey + date.getUTCHours() + this.token).toString() === data.token) {
-		return true;
+	if (MD5(powKey + date.getUTCHours() + this.token).toString() !== data.token) {
+		return false;
 	}
 
-	return false;
+	createVerificationRutine(license, macAddress, this.failCb);
+	return true;
 }
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+const createVerificationRutine = async (lic, macHash, cb) => {
+	while (true) {
+		await sleep(900000);
+		let logged = await loginWithLicense({ license: lic, macAddress: macHash });
+		if (!logged) {
+			cb();
+		}
+	}
+};
